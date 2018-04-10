13e2b73b33e4e02ab1cc91f2940f1dc8f78c0555
Thomas Schnellhardt <SchnelTh@BAIT.BBW.DD>
2018-04-10 16:03:52 +0200

    Fröhlich fertig bis auf Test

---
+++ königfröhlich/königfröhlich/FrohClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace königfröhlich
+{
+    public class FrohClass
+    {
+        public static bool Start_Frohtester(int testzahl)
+        {
+            var aufruf = Frohtester(testzahl);
+            return aufruf;
+        }
+
+        internal static bool Frohtester(int testzahl)
+        {
+            while (testzahl != 4 && testzahl != 1)
+            {
+                string zahlenstring = testzahl.ToString();
+                List<int> ziffern = new List<int>();
+                testzahl = 0;
+
+                foreach (char ziffer in zahlenstring)
+                {
+                    ziffern.Add(Convert.ToInt32(ziffer.ToString()));
+                }
+
+                foreach (int i in ziffern)
+                {
+                    testzahl = testzahl + Convert.ToInt32(Math.Sqrt(i));
+                }
+            }
+
+            bool wert;
+
+            if (testzahl == 1)
+            {
+                wert = true;
+            }
+            else
+            {
+                wert = false;
+            }
+            return wert;
+        }
+    }
+}
