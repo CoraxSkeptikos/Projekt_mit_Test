29786f33abbf41faf15cd625ac4ca91b5f288273
Thomas Schnellhardt <SchnelTh@BAIT.BBW.DD>
2018-04-12 14:44:55 +0200

    Zusammenführung mit Hilfe eines Wörterbuchs

--- Romanisch/Romanisch/ClassisRomana.cs
+++ Romanisch/Romanisch/ClassisRomana.cs
@@ -10,31 +10,28 @@ namespace Romanisch
     {
         public static string Initium(int numerus_barbarus)
         {
-            var mille = Translator.Divide(numerus_barbarus, 1000);
+            int[] spacium = new int[7];
+            spacium[0] = Translator.Divide(numerus_barbarus, 1000);
             numerus_barbarus = Translator.Modulo(numerus_barbarus, 1000);
 
-            var quingeni = Translator.Divide(numerus_barbarus, 500);
+            spacium[1] = Translator.Divide(numerus_barbarus, 500);
             numerus_barbarus = Translator.Modulo(numerus_barbarus, 500);
 
-            var centum = Translator.Divide(numerus_barbarus, 100);
+            spacium[2] = Translator.Divide(numerus_barbarus, 100);
             numerus_barbarus = Translator.Modulo(numerus_barbarus, 100);
 
-            var quinquaginta = Translator.Divide(numerus_barbarus, 50);
+            spacium[3] = Translator.Divide(numerus_barbarus, 50);
             numerus_barbarus = Translator.Modulo(numerus_barbarus, 50);
 
-            var decem = Translator.Divide(numerus_barbarus, 10);
+            spacium[4] = Translator.Divide(numerus_barbarus, 10);
             numerus_barbarus = Translator.Modulo(numerus_barbarus, 10);
 
-            var quinque = Translator.Divide(numerus_barbarus, 5);
-            numerus_barbarus = Translator.Modulo(numerus_barbarus, 5);
+            spacium[5] = Translator.Divide(numerus_barbarus, 5);
+            spacium[6] = Translator.Modulo(numerus_barbarus, 5);
 
-            var numerus_romanus = Translator.Adde_Mili(mille);
-            numerus_romanus = Translator.Adde_Quingeno(numerus_romanus, quingeni);
-            numerus_romanus = Translator.Adde_Centi(numerus_romanus, centum);
-            numerus_romanus = Translator.Adde_Quinquaginto(numerus_romanus, quinquaginta);
-            numerus_romanus = Translator.Adde_Deco(numerus_romanus, decem);
-            numerus_romanus = Translator.Adde_Quinquo(numerus_romanus, quinque);
-            numerus_romanus = Translator.Adde_Una(numerus_romanus, numerus_barbarus);
+
+            var vocabularium = Translator.Congerere_Vocabularium(spacium);
+            var numerus_romanus = Translator.Congerere_Numerus_Romanus(vocabularium);
 
             numerus_romanus = Simplificator.Unicum(numerus_romanus);
 
--- Romanisch/Romanisch/Translator.cs
+++ Romanisch/Romanisch/Translator.cs
@@ -20,75 +20,36 @@ namespace Romanisch
             return numerus_barbarus;
         }
 
-        internal static string Adde_Mili(int mille)
+        internal static Dictionary<string, int> Congerere_Vocabularium(int[] spacium)
+        {
+            var vocabularium = new Dictionary<string, int>();
+
+            vocabularium.Add("M", spacium[0]);
+            vocabularium.Add("D", spacium[1]);
+            vocabularium.Add("C", spacium[2]);
+            vocabularium.Add("L", spacium[3]);
+            vocabularium.Add("X", spacium[4]);
+            vocabularium.Add("V", spacium[5]);
+            vocabularium.Add("I", spacium[6]);
+
+            return vocabularium;
+        }
+
+        internal static string Congerere_Numerus_Romanus(Dictionary<string, int> vocabularium)
         {
             string numerus_romanus = "";
-            while (mille > 0)
+            foreach (KeyValuePair<string, int> res in vocabularium)
             {
-                numerus_romanus += "M";
-                mille--;
+                int iteratio = res.Value;
+                while (iteratio > 0)
+                {
+                    numerus_romanus += res.Key;
+                    iteratio--;
+                }
             }
+
             return numerus_romanus;
         }
 
-        internal static string Adde_Quingeno(string numerus_romanus, int quingeni)
-        {
-            while (quingeni > 0)
-            {
-                numerus_romanus += "D";
-                quingeni--;
-            }
-            return numerus_romanus;
-        }
-
-        internal static string Adde_Centi(string numerus_romanus, int centum)
-        {
-            while (centum > 0)
-            {
-                numerus_romanus += "C";
-                centum--;
-            }
-            return numerus_romanus;
-        }
-
-        internal static string Adde_Quinquaginto(string numerus_romanus, int quinquaginta)
-        {
-            while (quinquaginta > 0)
-            {
-                numerus_romanus += "L";
-                quinquaginta--;
-            }
-            return numerus_romanus;
-        }
-
-        internal static string Adde_Deco(string numerus_romanus, int decem)
-        {
-            while (decem > 0)
-            {
-                numerus_romanus += "X";
-                decem--;
-            }
-            return numerus_romanus;
-        }
-
-        internal static string Adde_Quinquo(string numerus_romanus, int quinque)
-        {
-            while (quinque > 0)
-            {
-                numerus_romanus += "V";
-                quinque--;
-            }
-            return numerus_romanus;
-        }
-
-        internal static string Adde_Una(string numerus_romanus, int una)
-        {
-            while (una > 0)
-            {
-                numerus_romanus += "I";
-                una--;
-            }
-            return numerus_romanus;
-        }
     }
 }
