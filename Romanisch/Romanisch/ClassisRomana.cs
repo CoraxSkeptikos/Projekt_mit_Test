4c1d7e227f02d032e2b4639cf0ec937c9be3aff0
Thomas Schnellhardt <SchnelTh@BAIT.BBW.DD>
2018-04-12 08:47:54 +0200

    Tausenderstellen noch ungetestet

---
+++ Romanisch/Romanisch/ClassisRomana.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Romanisch
+{
+    public class ClassisRomana
+    {
+        public static string Initium(int numerus_barbarus)
+        {
+            var expletium = Translator.Primum(numerus_barbarus);
+            return expletium;
+            
+        }
+    }
+}
---
+++ Romanisch/Romanisch/Translator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Romanisch
+{
+    class Translator
+    {
+        internal static string Primum(int numerus_barbarus)
+        {
+            int mille = numerus_barbarus / 1000;
+            numerus_barbarus -= mille;
+            string numerus_romanus = "";
+            while (mille > 0)
+            {
+                numerus_romanus += "M";
+                mille--;
+            }
+            return numerus_romanus;
+
+        }
+    }
+}
