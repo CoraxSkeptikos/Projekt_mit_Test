83d7ff9f4c2bc214d162ab406ce9855e4ab5f057
Thomas Schnellhardt <SchnelTh@BAIT.BBW.DD>
2018-04-13 08:42:23 +0200

    Erster Test noch mit statischen Werten

---
+++ Kalender/Kalender/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kalenderbibliothek;
+
+namespace Kalender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (Console.ReadLine().Contains("cal"))
+                {
+                    Kalenderhintergrund.Starte_Kalender().ForEach(Console.WriteLine);
+                }
+            }
+        }
+    }
+}
---
+++ Kalender/Kalenderbibliothek/Kalenderhintergrund.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalenderbibliothek
+{
+    public class Kalenderhintergrund
+    {
+        public static List<string> Starte_Kalender()
+        {
+            var ausgabe = new List<string>();
+
+            ausgabe.Add("     April 2018     ");
+            ausgabe.Add("So Mo Di Mi Do Fr Sa");
+            ausgabe.Add(" 1  2  3  4  5  6  7");
+            ausgabe.Add(" 8  9 10 11 12 13 14");
+            ausgabe.Add("15 16 17 18 19 20 21");
+            ausgabe.Add("22 23 24 25 26 27 28");
+            ausgabe.Add("29 30               ");
+
+            return ausgabe;
+        }
+    }
+}
