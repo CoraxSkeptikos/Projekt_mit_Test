7ca84c6347ebd0b8ad5ceb0e61fa9519378a0839
Thomas Schnellhardt <SchnelTh@BAIT.BBW.DD>
2019-01-11 13:13:29 +0100

    ASP.NET MVC Testprojekte erstellt
    Probleme mit IEnumerable

---
+++ meinUmfrageTool/meinUmfrageTool/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using meinUmfrageTool.Models;
+
+namespace meinUmfrageTool.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        private static FragebogenModel generischerFragebogen = new FragebogenModel
+        {
+            Fragen = new List<FragenModel> { new FragenModel() }
+        };
+
+        private FragebogenModel _fragebogen;
+
+        private FragebogenModel Fragebogen
+        {
+            get
+            {
+                if (_fragebogen == null)
+                {
+                    _fragebogen = Session["Fragebogen"] as FragebogenModel;
+                }
+                return _fragebogen;
+            }
+            set
+            {
+                _fragebogen = value;
+                Session["Fragebogen"] = _fragebogen;
+            }
+        }
+
+        public ActionResult FragebogenErstellen()
+        {
+            ViewBag.Message = "Erstellen Sie einen neuen Fragebogen";
+
+            if (Fragebogen == null)
+            {
+                Fragebogen = generischerFragebogen;
+            }
+
+            return View(Fragebogen);
+        }
+
+        [HttpPost]
+        public ActionResult FragebogenErstellen(FragebogenModel fragebogen, string befehl)
+        {
+            try
+            {
+                if (befehl == "Frage hinzufuegen")
+                {
+                    fragebogen.Fragen.Add(new FragenModel());
+                    Fragebogen = fragebogen;
+                }
+                return RedirectToAction("FragebogenErstellen");
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
+        }
+    }
+}
---
+++ meinUmfrageTool/meinUmfrageTool/Models/FragebogenModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace meinUmfrageTool.Models
+{
+    public class FragebogenModel
+    {
+        public FragebogenModel()
+        {
+            Fragen = new List<FragenModel>();
+        }
+
+        public string Bezeichnung { get; set; }
+
+        public List<FragenModel> Fragen { get; set; }
+    }
+}
